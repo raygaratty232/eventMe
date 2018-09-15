020d00138c811ba3146429923091d06e12475dd2
raygaratty232 <garatty.1411@gmail.com>
2018-09-16 00:16:12 +0300

    more commit

--- js.js
+++ js.js
@@ -1,6 +1,16 @@
 var enterActive = document.querySelector('#enter-active');
 var formCont = document.querySelector('.form-cont');
+var lanBtn = document.querySelector('#lan-btn');
+var languageBlock = document.querySelector('.language-block');
 
 enterActive.addEventListener('click', function(){
 	formCont.classList.toggle('open')
+})
+
+
+lanBtn.addEventListener('click', function(){
+	languageBlock.classList.toggle('show');
+	languageBlock.addEventListener('transitionend', function(){
+		console.log('1');
+	})
 })
